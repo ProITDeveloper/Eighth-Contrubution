b8bca5816601f32393bfc0513fa2d4766e0b9029
Pro. IT-Developer <44401041+ProITDeveloper@users.noreply.github.com>
2018-10-30 00:36:29 +0500

    Add this to Eighth-Contribution

--- load.php
+++ load.php
@@ -4,3 +4,5 @@ include_once 'lib/qode-twitter-api.php';
 include_once 'widgets/load.php';
 require_once 'shortcodes/qode-twitter-feed.php';
 include_once 'shortcodes/load.php';
+
+<--This is comment-->
